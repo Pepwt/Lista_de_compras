f47916573a57abb3f705a5f2b79ee026ac8be4fa
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-22 22:10:17 -0300

    first commit

---
+++ app/src/main/java/br/com/fiap/listadecomprass/MainActivity.kt
@@ -0,0 +1,29 @@
+package br.com.fiap.listadecomprass
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.listadecomprass.ui.theme.ListadecomprassTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Lista de Compras"
+
+    }
+}
+
